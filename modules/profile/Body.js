73e0a5e1f83abdfc29420335e477c2797d11f8c3
Kamil Dobrzyński <kamil.dobrzynski@ithero.pl>
2019-06-11 15:08:24 +0200

    Add plants List functionality

--- modules/profile/Body.js
+++ modules/profile/Body.js
@@ -1,31 +1,59 @@
 import React, { Component } from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import { Card, Slider } from 'react-native-elements';
+import { StyleSheet, Text, View } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient/index';
+import PlantsList from './components/PlantsList';
 
 export default class Body extends Component {
-  state = { isCardVisible: true };
-
-  componentDidMount() {
-    this.setState({
-      isCardVisible: true,
-    });
+  constructor(props) {
+    super(props);
+    this.state = {
+      plantsList: [],
+    };
   }
 
-  deleteCard = () => {
-    this.setState({
-      isCardVisible: false,
-    });
+  deleteCard = (id) => {
+    const { plantsList } = this.state;
+
+    const filteredData = plantsList.filter(item => item.id !== id);
+    this.setState({ plantsList: filteredData });
   };
 
   addCard = () => {
-    this.setState({
-      isCardVisible: true,
-    });
+    const newItem = {
+      id: this.state.plantsList.length,
+      title: `Plant #${this.state.plantsList.length}`,
+      image: require('../../assets/images/plant1.jpeg'),
+    };
+
+    const array = this.state.plantsList;
+
+    array.push(newItem);
+
+    this.setState({ plantsList: [...array] });
   };
 
+  _renderAddPlantHolder = () => (
+    <LinearGradient
+      colors={['#116e1c', '#092509']}
+      start={{ x: 0.0, y: 1.0 }}
+      end={{ x: 1.0, y: 1.0 }}
+      style={{
+        height: 50,
+        width: '100%',
+        alignItems: 'center',
+        borderRadius: 0,
+        justifyContent: 'center',
+        marginTop: 0,
+      }}
+    >
+      <Text style={styles.addPlantTxt} onPress={this.addCard}>
+        ADD PLANT
+      </Text>
+    </LinearGradient>
+  );
+
   render() {
-    const { isCardVisible } = this.state;
+    const { plantsList } = this.state;
     return (
       <View style={styles.container}>
         <LinearGradient
@@ -38,182 +66,11 @@ export default class Body extends Component {
             justifyContent: 'center',
           }}
         >
-          <LinearGradient
-            colors={['#116e1c', '#092509']}
-            start={{ x: 0.0, y: 1.0 }}
-            end={{ x: 1.0, y: 1.0 }}
-            style={{
-              height: 50,
-              width: '100%',
-              alignItems: 'center',
-              borderRadius: 0,
-              justifyContent: 'center',
-              marginTop: 0,
-            }}
-          >
-            <Text style={styles.addPlantTxt} onPress={this.addCard}>
-              ADD PLANT
-            </Text>
-          </LinearGradient>
-          <ScrollView style={{ width: '100%' }}>
-            <View>
-              {isCardVisible && [
-                <Card
-                  containerStyle={{
-                    height: 325,
-                  }}
-                  key="card"
-                  flexDirection="column"
-                  title="Plant #1"
-                  titleStyle={{
-                    color: 'green',
-                  }}
-                  image={require('../../assets/images/plant1.jpeg')}
-                  imageStyle={{
-                    height: 150,
-                    marginTop: -10,
-                  }}
-                >
-                  <View
-                    style={{
-                      flex: 1,
-                      alignItems: 'center',
-                      flexDirection: 'row',
-                    }}
-                  >
-                    <LinearGradient
-                      colors={['#00b3b7', '#084d7a']}
-                      start={{ x: 0.0, y: 1.0 }}
-                      end={{ x: 1.0, y: 1.0 }}
-                      style={{
-                        height: 40,
-                        width: 120,
-                        alignItems: 'center',
-                        borderRadius: 25,
-                        justifyContent: 'center',
-                        marginRight: 210,
-                        marginBottom: 90,
-                      }}
-                    >
-                      <Text style={styles.addPlantTxt}>PODLEJ</Text>
-                    </LinearGradient>
-                    <LinearGradient
-                      colors={['#00b3b7', '#084d7a']}
-                      start={{ x: 0.0, y: 1.0 }}
-                      end={{ x: 1.0, y: 1.0 }}
-                      style={{
-                        height: 40,
-                        width: 120,
-                        alignItems: 'center',
-                        borderRadius: 25,
-                        justifyContent: 'center',
-                        marginLeft: -100,
-                        marginBottom: 90,
-                      }}
-                    >
-                      <Text
-                        style={styles.addPlantTxt}
-                        onPress={this.deleteCard}
-                      >
-                        USUN
-                      </Text>
-                    </LinearGradient>
-                  </View>
-                  <Text>Wilgoć</Text>
-                  <Slider
-                    minimumValue={0}
-                    maximumValue={100}
-                    value={50}
-                    thumbTintColor="green"
-                    minimumTrackTintColor="green"
-                    maximumTrackTintColor="black"
-                  />
-                  <Text>Temperatura</Text>
-                  <Slider
-                    minimumValue={0}
-                    maximumValue={100}
-                    value={50}
-                    thumbTintColor="orange"
-                    minimumTrackTintColor="orange"
-                    maximumTrackTintColor="red"
-                  />
-                </Card>,
-              ]}
-              <Card
-                containerStyle={{
-                  height: 325,
-                }}
-                key="card"
-                flexDirection="column"
-                title="Plant #2"
-                titleStyle={{
-                  color: 'green',
-                }}
-                image={require('../../assets/images/plant1.jpeg')}
-                imageStyle={{
-                  height: 150,
-                  marginTop: -10,
-                }}
-              >
-                <View
-                  style={{ flex: 1, alignItems: 'center', flexDirection: 'row' }}
-                >
-                  <LinearGradient
-                    colors={['#00b3b7', '#084d7a']}
-                    start={{ x: 0.0, y: 1.0 }}
-                    end={{ x: 1.0, y: 1.0 }}
-                    style={{
-                      height: 40,
-                      width: 120,
-                      alignItems: 'center',
-                      borderRadius: 25,
-                      justifyContent: 'center',
-                      marginRight: 210,
-                      marginBottom: 90,
-                    }}
-                  >
-                    <Text style={styles.addPlantTxt}>PODLEJ</Text>
-                  </LinearGradient>
-                  <LinearGradient
-                    colors={['#00b3b7', '#084d7a']}
-                    start={{ x: 0.0, y: 1.0 }}
-                    end={{ x: 1.0, y: 1.0 }}
-                    style={{
-                      height: 40,
-                      width: 120,
-                      alignItems: 'center',
-                      borderRadius: 25,
-                      justifyContent: 'center',
-                      marginLeft: -100,
-                      marginBottom: 90,
-                    }}
-                  >
-                    <Text style={styles.addPlantTxt} onPress={this.deleteCard}>
-                      USUN
-                    </Text>
-                  </LinearGradient>
-                </View>
-                <Text>Wilgoć</Text>
-                <Slider
-                  minimumValue={0}
-                  maximumValue={100}
-                  value={50}
-                  thumbTintColor="green"
-                  minimumTrackTintColor="green"
-                  maximumTrackTintColor="black"
-                />
-                <Text>Temperatura</Text>
-                <Slider
-                  minimumValue={0}
-                  maximumValue={100}
-                  value={50}
-                  thumbTintColor="orange"
-                  minimumTrackTintColor="orange"
-                  maximumTrackTintColor="red"
-                />
-              </Card>
-            </View>
-          </ScrollView>
+          {this._renderAddPlantHolder()}
+
+          <View style={styles.plantsList}>
+            <PlantsList data={plantsList} delete={this.deleteCard} />
+          </View>
         </LinearGradient>
       </View>
     );
@@ -225,10 +82,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'rgba(255,255,255, 0.4)',
   },
-
   addPlantTxt: {
     fontSize: 15,
     color: '#fff',
     textAlign: 'center',
+  },
+  plantsList: {
+    flex: 1,
   },
 });
